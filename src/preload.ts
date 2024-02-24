2e724bae7c91728b599fdda984ef62738e475aa5
dynilath <dynilath@qq.com>
2024-02-24 22:20:11 +0800

    fix add Dexie globals, close #2

--- src/preload.ts
+++ src/preload.ts
@@ -14,17 +14,29 @@ window.addEventListener("DOMContentLoaded", () => {
 interface Window {
     CommonGetServer?: () => string;
     exports?: any;
+    Dexie?: any;
+}
+
+function addScript(src: string, type?: string) {
+    let onload: (() => void) | undefined;
+
+    const script = document.createElement('script');
+    script.src = src;
+    if (type) script.type = type;
+    script.onload = () => onload?.();
+    document.head.appendChild(script);
+
+    return { onload: (cb: () => void) => { onload = cb; } };
 }
 
 window.addEventListener('DOMContentLoaded', () => {
     if (window.exports === undefined) window.exports = {};
 
-    const script = document.createElement('script');
-    script.src = "../../build/renderer.js";
-    script.type = "module";
-    document.head.appendChild(script);
+    addScript("../../build/renderer.js", "module").onload(() => {
+        window.Dexie = require('dexie');
+    });
 
-    window.CommonGetServer = () => {
+    (window as Window).CommonGetServer = () => {
         console.log('CommonGetServer');
         return "https://bondage-club-server.herokuapp.com/";
     }

e2ddcb2c2adcf0b2992a231527d54aeb40d18fcd
dynilath <dynilath@qq.com>
2024-10-23 03:15:17 +0800

    chore: dependencies for planned feature

--- src/preload.ts
+++ src/preload.ts
@@ -1,16 +1,3 @@
-window.addEventListener("DOMContentLoaded", () => {
-    const replaceText = (selector: string, text: string) => {
-        const element = document.getElementById(selector);
-        if (element) {
-            element.innerText = text;
-        }
-    };
-
-    for (const type of ["chrome", "node", "electron"]) {
-        replaceText(`${type}-version`, process.versions[type as keyof NodeJS.ProcessVersions] as string);
-    }
-});
-
 interface Window {
     CommonGetServer?: () => string;
     exports?: any;

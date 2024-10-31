eebb1d257cb61109b1194ed41f1d46ec10f2b74f
dynilath <dynilath@qq.com>
2024-10-31 11:56:02 +0800

    build: fix auto release script

--- scripts/.gh-release.js
+++ scripts/.gh-release.js
@@ -5,59 +5,59 @@ const TAG_NAME = `v${require("../package.json").version}`;
 setEnv();
 
 function checkTagExists(tag) {
-  return new Promise(async (resolve, reject) => {
-    const response = await axios.get(
-      `https://api.github.com/repos/${process.env.GITHUB_REPOSITORY}/releases/tags/${tag}`,
-      {
-        headers: {
-          Authorization: `token ${process.env.GH_TOKEN}`,
-          Accept: "application/vnd.github+json",
-        },
-      }
-    );
+  const url = `https://api.github.com/repos/${process.env.GITHUB_REPOSITORY}/releases`;
+  const config = {
+    headers: {
+      Authorization: `token ${process.env.GH_TOKEN}`,
+      Accept: "application/vnd.github+json",
+    },
+  };
 
-    if (!response.data.draft) reject("Release is not a draft");
+  return new Promise(async (resolve, reject) =>
+    axios
+      .get(url, config)
+      .then((response) => {
+        const target = response.data.find(
+          (release) => release.tag_name === tag
+        );
 
-    if (
-      !response.data.assets.some(
-        (asset) => asset.name.endsWith(".exe") && !asset.name.includes("delta")
-      )
-    )
-      reject("No exe file found in the release");
-
-    resolve({ data: response.data });
-  });
+        if (!target) reject("Release does not exist");
+        else return target;
+      })
+      .then((target) => axios.get(target.url, config))
+      .then((response) => {
+        const { draft } = response.data;
+        if (!draft) reject("Release is not a draft");
+        else resolve(response.data);
+      })
+      .catch(reject)
+  );
 }
 
-async function createRelease(tag) {
+async function createRelease({ tag_name, name, url }) {
   const body = {
-    tag_name: tag,
-    name: tag,
-    body: `Release for version ${tag}`,
+    tag_name: tag_name,
+    name: name,
+    body: `Release for version ${name}`,
     draft: false,
     prerelease: false,
     make_newest: "legacy",
   };
 
-  const response = await axios.patch(
-    `https://api.github.com/repos/${process.env.GITHUB_REPOSITORY}/releases/${tag}`,
-    body,
-    {
-      headers: {
-        Authorization: `token ${process.env.GH_TOKEN}`,
-        Accept: "application/vnd.github+json",
-      },
-    }
-  );
+  const config = {
+    headers: {
+      Authorization: `token ${process.env.GH_TOKEN}`,
+      Accept: "application/vnd.github+json",
+    },
+  };
 
-  return response.data.html_url; // 返回创建的 Release 数据
+  const response = await axios.patch(url, body, config);
+
+  return response.data.html_url;
 }
 
 (async () => {
   await checkTagExists(TAG_NAME)
-    .then(({ tag_name }) => createRelease(tag_name))
-    .then(
-      (data) => console.log(data),
-      (error) => console.error(error)
-    );
+    .then((data) => createRelease(data))
+    .then(console.log, console.error);
 })();

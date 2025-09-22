5232a859882cbb17734e501c2987530774d7aaa5
dynilath <dynilath@qq.com>
2025-09-23 01:30:01 +0800

    refactor: unify quote styles and improve formatting in handler and WindowState files

--- src/handler.ts
+++ src/handler.ts
@@ -1,42 +1,42 @@
-import { ipcMain } from "electron";
-import { BCURLPreference } from "./urlprefer";
+import { ipcMain } from 'electron'
+import { BCURLPreference } from './urlprefer'
 
-const LoadedEvent = "page-loaded";
+const LoadedEvent = 'page-loaded'
 
-function handler(webContents: Electron.WebContents, then: () => void) {
+function handler (webContents: Electron.WebContents, then: () => void) {
   const ret = (event: Electron.IpcMainEvent) => {
     if (event.sender.id === webContents.id) {
-      ipcMain.removeListener(LoadedEvent, ret);
-      then();
+      ipcMain.removeListener(LoadedEvent, ret)
+      then()
     }
-  };
-  return ret;
+  }
+  return ret
 }
 
 export class ContentLoadState {
-  private _loaded = false;
-  constructor(readonly webContents: Electron.WebContents) {
+  private _loaded = false
+  constructor (readonly webContents: Electron.WebContents) {
     ipcMain.on(
       LoadedEvent,
       handler(webContents, () => {
-        this._loaded = true;
+        this._loaded = true
       })
-    );
+    )
   }
 
-  loaded() {
-    if (this._loaded) return Promise.resolve();
-    return new Promise<void>((resolve) => {
-      ipcMain.once(LoadedEvent, handler(this.webContents, resolve));
-    });
+  loaded () {
+    if (this._loaded) return Promise.resolve()
+    return new Promise<void>(resolve => {
+      ipcMain.once(LoadedEvent, handler(this.webContents, resolve))
+    })
   }
 
-  reload() {
-    this._loaded = false;
-    this.webContents.loadURL(BCURLPreference.choice.url);
-    console.log("Reload page");
-    return new Promise<void>((resolve) => {
-      ipcMain.once(LoadedEvent, handler(this.webContents, resolve));
-    });
+  reload () {
+    this._loaded = false
+    this.webContents.loadURL(BCURLPreference.choice.url)
+    console.log('Reload page')
+    return new Promise<void>(resolve => {
+      ipcMain.once(LoadedEvent, handler(this.webContents, resolve))
+    })
   }
 }
--- src/main/WindowState.ts
+++ src/main/WindowState.ts
@@ -1,65 +1,65 @@
-import { BrowserWindow } from 'electron';
-import settings from 'electron-settings';
+import { BrowserWindow } from 'electron'
+import settings from 'electron-settings'
 
 type WinStateType = {
-    x?: number,
-    y?: number,
-    width: number,
-    height: number,
-    isMaximized?: boolean,
-};
-
-function _bounds(windowName: string): WinStateType | null {
-  return settings.getSync(`windowState.${windowName}`) as WinStateType | null;
+  x?: number
+  y?: number
+  width: number
+  height: number
+  isMaximized?: boolean
 }
 
-function _save(windowName: string, windowState: WinStateType) {
-  settings.setSync(`windowState.${windowName}`, windowState);
+function _bounds (windowName: string): WinStateType | null {
+  return settings.getSync(`windowState.${windowName}`) as WinStateType | null
+}
+
+function _save (windowName: string, windowState: WinStateType) {
+  settings.setSync(`windowState.${windowName}`, windowState)
 }
 
 export class StateKeptWindow {
-  windowState: WinStateType;
-  window: BrowserWindow;
+  windowState: WinStateType
+  window: BrowserWindow
 
-  constructor(
+  constructor (
     readonly windowName: string,
     readonly option?: Electron.BrowserWindowConstructorOptions
   ) {
     const bounds = _bounds(windowName) || {
       width: 1000,
       height: 800,
-    };
-    const nOption = Object.assign(option || {}, bounds);
-    this.window = new BrowserWindow(nOption);
-    if (bounds.isMaximized) this.window.maximize();
-    this.windowState = bounds;
+    }
+    const nOption = Object.assign(option || {}, bounds)
+    this.window = new BrowserWindow(nOption)
+    if (bounds.isMaximized) this.window.maximize()
+    this.windowState = bounds
     const save = () => {
-      this.windowState = this.window.getBounds();
-      this.windowState.isMaximized = this.window.isMaximized();
-      _save(windowName, this.windowState);
-    };
-    this.window.on("resize", save);
-    this.window.on("move", save);
-    this.window.on("close", save);
+      this.windowState = this.window.getBounds()
+      this.windowState.isMaximized = this.window.isMaximized()
+      _save(windowName, this.windowState)
+    }
+    this.window.on('resize', save)
+    this.window.on('move', save)
+    this.window.on('close', save)
   }
 
-  get webContents() {
-    return this.window.webContents;
+  get webContents () {
+    return this.window.webContents
   }
 
-  loadURL(...args: Parameters<BrowserWindow["loadURL"]>) {
-    this.window.loadURL(...args);
+  loadURL (...args: Parameters<BrowserWindow['loadURL']>) {
+    this.window.loadURL(...args)
   }
 
-  loadFile(...args: Parameters<BrowserWindow["loadFile"]>) {
-    this.window.loadFile(...args);
+  loadFile (...args: Parameters<BrowserWindow['loadFile']>) {
+    this.window.loadFile(...args)
   }
 
-  on(...args: Parameters<BrowserWindow["on"]>) {
-    this.window.on(...args);
+  on (...args: Parameters<BrowserWindow['on']>) {
+    this.window.on(...args)
   }
 
-  close() {
-    this.window.close();
+  close () {
+    this.window.close()
   }
 }

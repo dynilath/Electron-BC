af991bb2666ae8eee290d35ee1cba5c26da9f21d
dynilath <dynilath@qq.com>
2026-01-24 22:24:19 +0800

    feat: display url in the title bar

--- src/main/AppMenu/menu.ts
+++ src/main/AppMenu/menu.ts
@@ -1,59 +1,61 @@
-import { Menu } from "electron";
-import { scriptMenu } from "./script";
-import { MyAppMenuConstructorOption } from "./type";
-import { builtinMenu } from "./builtins";
-import { aboutMenu } from "./about";
-import { cacheMenu } from "./cache";
-import { DoH } from "../DoH";
+import { Menu } from 'electron';
+import { scriptMenu } from './script';
+import { MyAppMenuConstructorOption } from './type';
+import { builtinMenu } from './builtins';
+import { aboutMenu } from './about';
+import { cacheMenu } from './cache';
+import { DoH } from '../DoH';
 
 export function makeMenu(options: MyAppMenuConstructorOption) {
   const { refreshPage, parent } = options;
 
   const { window, i18n } = parent;
 
+  window.title = `Bondage Club - ${options.BCVersion.url}`;
+
   return Menu.buildFromTemplate([
     {
-      label: i18n("MenuItem::Tools"),
-      id: "tools" as AppMenuIds,
+      label: i18n('MenuItem::Tools'),
+      id: 'tools' as AppMenuIds,
       submenu: [
         {
-          label: i18n("MenuItem::Tools::Refresh"),
-          type: "normal",
-          accelerator: "F5",
+          label: i18n('MenuItem::Tools::Refresh'),
+          type: 'normal',
+          accelerator: 'F5',
           click: () => refreshPage(),
         },
         {
-          label: i18n("MenuItem::Tools::FullScreen"),
-          type: "normal",
-          accelerator: "F11",
+          label: i18n('MenuItem::Tools::FullScreen'),
+          type: 'normal',
+          accelerator: 'F11',
           click: () => window.setFullScreen(!window.isFullScreen()),
         },
         {
-          label: i18n("MenuItem::Tools::DevTools"),
-          type: "normal",
-          accelerator: "F12",
+          label: i18n('MenuItem::Tools::DevTools'),
+          type: 'normal',
+          accelerator: 'F12',
           click: () => window.webContents.toggleDevTools(),
         },
         {
-          type: "separator",
+          type: 'separator',
         },
         ...cacheMenu(options),
         {
-          type: "separator",
+          type: 'separator',
         },
         {
-          label: i18n("MenuItem::Tools::OpenDoHConfigFile"),
-          sublabel: i18n("MenuItem::Tools::DoHConfigTips"),
-          type: "normal",
+          label: i18n('MenuItem::Tools::OpenDoHConfigFile'),
+          sublabel: i18n('MenuItem::Tools::DoHConfigTips'),
+          type: 'normal',
           click: () => DoH.openConfigFile(),
         },
         {
-          type: "separator",
+          type: 'separator',
         },
         {
-          label: i18n("MenuItem::Tools::Exit"),
-          type: "normal",
-          accelerator: "Alt+F4",
+          label: i18n('MenuItem::Tools::Exit'),
+          type: 'normal',
+          accelerator: 'Alt+F4',
           click: () => window.close(),
         },
       ],

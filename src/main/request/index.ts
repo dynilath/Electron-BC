8ba3de45d35dadc2e36fa0c3c2c3b5376b62af5d
dynilath <dynilath@qq.com>
2026-02-25 23:52:57 +0800

    feat: better cache support for echo mods

--- src/main/request/index.ts
+++ src/main/request/index.ts
@@ -1,46 +1,46 @@
-import { requestAssetResponse } from './utils'
+import { requestAssetResponse } from './utils';
 
 export interface ResourceHandler<T extends object = object> {
-  willHandle(request: Request): false | T
-  handle(request: Request, context: T): Promise<Response>
+  willHandle(request: Request): false | T;
+  handle(request: Request, context: T): Promise<Response>;
 }
 
 export interface ProtocolSetting {
-  url: string
-  version: string
+  url: string;
+  version: string;
 }
 
-export class BCResourceHandler
-  implements ResourceHandler<{ assetKey: string }>
-{
-  bcStatus?: ProtocolSetting[]
-  constructor (bcStatus?: ProtocolSetting[]) {
-    this.bcStatus = bcStatus
+export class BCResourceHandler implements ResourceHandler<{
+  assetKey: string;
+}> {
+  bcStatus?: ProtocolSetting[];
+  constructor(bcStatus?: ProtocolSetting[]) {
+    this.bcStatus = bcStatus;
   }
 
-  private assetKey (request: Request) {
-    return request.url.substring(request.url.lastIndexOf('BondageClub/') + 12)
+  private assetKey(request: Request) {
+    return request.url.substring(request.url.lastIndexOf('BondageClub/') + 12);
   }
 
-  setBCStatus (bcStatus: ProtocolSetting[]) {
-    this.bcStatus = bcStatus
+  setBCStatus(bcStatus: ProtocolSetting[]) {
+    this.bcStatus = bcStatus;
   }
 
-  willHandle (request: Request) {
+  willHandle(request: Request) {
     if (
       this.bcStatus &&
       this.bcStatus.some(s => request.url.startsWith(s.url))
     ) {
-      const assetKey = this.assetKey(request)
-      const idx = assetKey.lastIndexOf('.')
-      const ext = idx === -1 ? '' : assetKey.substring(idx)
+      const assetKey = this.assetKey(request);
+      const idx = assetKey.lastIndexOf('.');
+      const ext = idx === -1 ? '' : assetKey.substring(idx);
       if (['.png', '.jpg', '.mp3', '.txt', '.ttf'].includes(ext))
-        return { assetKey }
+        return { assetKey };
     }
-    return false
+    return false;
   }
 
-  async handle (
+  async handle(
     request: Request,
     { assetKey }: { assetKey: string }
   ): Promise<Response> {
@@ -48,66 +48,83 @@ export class BCResourceHandler
       request.url,
       assetKey,
       this.bcStatus![0].version
-    )
+    );
   }
 }
 
-export class EchoResourceHandler
-  implements ResourceHandler<{ resource: string; version: string }>
-{
-  willHandle (request: Request) {
+export class EchoResourceHandler implements ResourceHandler<{
+  resource: string;
+  version: string;
+}> {
+  willHandle(request: Request) {
     const urls = [
-      'https://emdsa2.github.io/-mod/',
       'https://sugarchain-studio.github.io/echo-clothing-ext/',
       'https://sugarchain-studio.github.io/echo-activity-ext/',
-    ]
-    const url = urls.find(u => request.url.startsWith(u))
-    if (!url) return false
-    const [resource, args] = request.url.substring(url.length).split('?', 2)
-    if (args === undefined) return false
+      'https://cdn.jsdelivr.net/gh/SugarChain-Studio/echo-clothing-ext@',
+      'https://cdn.jsdelivr.net/gh/SugarChain-Studio/echo-activity-ext@',
+    ];
+    const base_url = urls.find(u => request.url.startsWith(u));
+    if (!base_url) return false;
+
+    let resource, version;
+
+    if (base_url.startsWith('https://sugarchain-studio.github.io/')) {
+      const vIdx = request.url.indexOf('?v=');
+      resource = request.url.substring(base_url.length, vIdx);
+      version = request.url.substring(vIdx + 3);
+    } else if (
+      base_url.startsWith('https://cdn.jsdelivr.net/gh/SugarChain-Studio/')
+    ) {
+      // https://cdn.jsdelivr.net/gh/SugarChain-Studio/echo-clothing-ext@abcedf1234567890/resources/Path.png
+      // '/resources/' is not part of resource url
+      const separator = '/resources/';
+      const shaEnd = request.url.indexOf(separator, base_url.length);
+      if (shaEnd === -1) return false;
+      version = request.url.substring(base_url.length, shaEnd);
+      resource = request.url.substring(
+        base_url.length + version.length + separator.length
+      );
+    }
+
+    if (!resource || !version) return false;
 
     if (
       resource.endsWith('.png') ||
       resource.endsWith('.jpg') ||
       resource.endsWith('.webp')
     ) {
-      const v_start = args.indexOf('v=')
-      const version = args.substring(
-        v_start + 2,
-        Math.min(v_start + 9, args.length)
-      )
-      return { resource, version }
+      return { resource, version };
     }
-    return false
+    return false;
   }
 
-  handle (
+  handle(
     request: Request,
     { resource, version }: { resource: string; version: string }
   ): Promise<Response> {
-    return requestAssetResponse(request.url, `EchoMod://${resource}`, version)
+    return requestAssetResponse(request.url, `EchoMod://${resource}`, version);
   }
 }
 
-export class MPAResourceHandler
-  implements ResourceHandler<{ version: string }>
-{
-  willHandle (request: Request) {
+export class MPAResourceHandler implements ResourceHandler<{
+  version: string;
+}> {
+  willHandle(request: Request) {
     if (
       request.url.startsWith('https://mayathefoxy.github.io/MPA/assets/') &&
       request.url.endsWith('.mp3')
     ) {
-      const parts = request.url.split('/')
-      const version = parts[parts.length - 1]
-      return { version }
+      const parts = request.url.split('/');
+      const version = parts[parts.length - 1];
+      return { version };
     }
-    return false
+    return false;
   }
 
-  handle (
+  handle(
     request: Request,
     { version }: { version: string }
   ): Promise<Response> {
-    return requestAssetResponse(request.url, `MPA://${request.url}`, version)
+    return requestAssetResponse(request.url, `MPA://${request.url}`, version);
   }
 }
